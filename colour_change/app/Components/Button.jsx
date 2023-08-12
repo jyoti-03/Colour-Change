ce138094ea49d28062daa4e7ce7177c34afb6546
Jyotirani Behera <jyotirani21@navgurukul.org>
2023-08-13 00:40:10 +0530

    Done All

---
+++ colour_change/app/Components/Button.jsx
@@ -0,0 +1,26 @@
+const Button = ({count}) => {
+  var incermentbtnG = () =>{
+    count("grey")
+  }
+
+  var incermentbtnT = () =>{
+    count("teal")
+  }
+
+  var incermentbtnR = () =>{
+    count("red")
+  }
+
+  var incermentbtnB = () =>{
+    count("blue")
+  }
+    return (
+      <div className="btnDiv">
+        <button className="bGrey" onClick={incermentbtnG} value="grey">Grey</button>
+        <button className="bTeal" onClick={incermentbtnT} value="teal">Teal</button>
+        <button className="bRed" onClick={incermentbtnR} value="red">Red</button>
+        <button className="bBlue" onClick={incermentbtnB} value="blue">Blue</button>
+      </div>
+    )
+}
+export default Button;
